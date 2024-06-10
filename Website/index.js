489850d76b42ae86dfd31bf19e0d100a38fdc613
iogwu-admin <iogwu@sspa.com>
2024-06-10 09:52:36 -0400

    update

--- Website/index.js
+++ Website/index.js
@@ -33,3 +33,31 @@ image.onclick = () => {
      }
 };
 
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+
+
+
+function setUserName(){
+     const name = prompt('Enter your Username.');
+     if(!name){
+          setUserName();
+     }
+     else{
+          localStorage.setItem('name', name);
+          myHeading.textContent = `Welcome, ${name}`;
+     }
+}
+
+if(!localStorage.getItem('name')){
+     setUserName();
+}else{
+     const storedName = localStorage.getItem('name');
+     myHeading.textContent = `Good morning ${storedName}`;
+}
+myButton.onclick = () => {
+     setUserName();
+};
