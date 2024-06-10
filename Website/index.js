f067744bc2c0479b81938a807504ddacb5c9d5bc
Israel Ogwu <57731260+israelo19@users.noreply.github.com>
2024-06-10 08:41:59 -0400

    Add files via upload

---
+++ Website/index.js
@@ -0,0 +1,35 @@
+// const heading = document.querySelector('h1');
+// heading.textContent = 'SSPA!';
+
+// const para = document.querySelector('p');
+// para.textContent = 'Why hello there!';
+
+// document.querySelector('html').addEventListener('click', () =>{
+//      alert('Ouch! Stop poking me!');
+// });
+
+// function setUserName(){
+//      const name = prompt('Enter Username.');
+//      if(!name){
+//           setUserName();
+//      }else{
+//           localStorage.setItem('name', name);
+//           myHeading.textContent = `Welcom to the website, ${name}`;
+//      }
+// }
+
+// setUserName();
+
+
+const image  = document.querySelector("img")
+
+image.onclick = () => {
+     const mySrc = image.getAttribute("src");
+     if(mySrc === "images/sspalogo.png"){
+          image.setAttribute("src", "images/256x256.png");
+     }
+     else{
+          image.setAttribute("src", "images/sspalogo.png");
+     }
+};
+
